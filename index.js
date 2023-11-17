8d9445ae08f15960ea2f7b80cfe0d5e23723f50f
Douglas Silva <silva.o.douglas@gmail.com>
2023-11-17 00:51:23 -0300

    feito código e alterado readme

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("ola mundo")
+calculate("Tony Stark", 70, 30)
+calculate("Thor", 130, 30)
+calculate("Homem Aranha", 20, 10)
+calculate("Homem Formiga", 20, 3)
+calculate("Loki", 60, 4)
+calculate("Doutor Estranho", 90, 6)
+calculate("Thanos", 300, 30)
+
+function calculate(player, wins, loses) {
+    let ranking = wins - loses
+    let ranked
+
+        if (ranking <= 10){
+            ranked = "Ferro"
+        }
+        else if (ranking >= 11 && ranking <= 20){
+            ranked = "Bronze"
+        }
+        else if (ranking >= 21 && ranking <= 50){
+            ranked = "Prata"
+        }
+        else if (ranking >= 51 && ranking <= 80){
+            ranked = "Ouro"
+        }
+        else if (ranking >= 81 && ranking <= 90){
+            ranked = "Diamante"
+        }
+        else if (ranking >= 91 && ranking <= 100){
+            ranked = "Lendário"
+        }
+        else if (ranking >= 101){
+            ranked = "Imortal"
+        }
+        console.log(`O Jogador ${player} tem ${wins} vitórias, ${loses} derrotas e está no ranking: ${ranked}`)
+}
